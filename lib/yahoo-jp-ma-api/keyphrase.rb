ac1cb470383994680cb889279bfb20f79120f2d1
rhysd <u033263f@ecs.cmc.osaka-u.ac.jp>
2012-04-13 18:58:28 +0900

    add KeyphraseService/V1/extarct API

---
+++ lib/yahoo-jp-ma-api/keyphrase.rb
@@ -0,0 +1,21 @@
+
+require 'yahoo-jp-ma-api/request'
+require 'rexml/document'
+
+module YahooMA
+    class Client
+        module Keyphrase
+            def keyphrase params
+                raise "keyphrase requires following params: sentence" unless  params.include? :sentence
+                res = request 'http://jlp.yahooapis.jp/KeyphraseService/V1/extract',app_key,params
+                keyphrases = []
+                doc = REXML::Document.new(body).elements['ResultSet/']
+                doc.elements.each('Result') do |result|
+                    keyphrases[result['Keyphrase'].text] = result['Score'].text.to_i
+                end
+                keyphrases
+            end
+        end
+    end
+end
+
