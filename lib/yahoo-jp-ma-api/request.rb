2be770216282a5445141934ea4cdd6cadb4aa0b0
rhysd <u033263f@ecs.cmc.osaka-u.ac.jp>
2012-05-02 06:31:35 +0900

    消し忘れていたデバッグコードを削除

--- lib/yahoo-jp-ma-api/request.rb
+++ lib/yahoo-jp-ma-api/request.rb
@@ -7,9 +7,6 @@ module YahooMA
         def request path,app_id,params={}
             uri = URI.parse path
             queries = 'appid='+app_id+'&'+params.map{|k,v| k.to_s+'='+v.to_s}.join('&')
-
-            puts "request: "+path+"?"+queries
-
             req = Net::HTTP::Post.new uri.path
             res = Net::HTTP.start(uri.host,uri.port) do |http|
                 http.request req, queries
