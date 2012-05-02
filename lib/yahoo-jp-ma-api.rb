2f943b983df7e1e4236de4a3aee16330b458fb53
rhysd <u033263f@ecs.cmc.osaka-u.ac.jp>
2012-05-02 20:26:45 +0900

    ゲートウェイとなるモジュール YahooMA の挙動を変更．reset で状態を初期化できるようにした．

--- lib/yahoo-jp-ma-api.rb
+++ lib/yahoo-jp-ma-api.rb
@@ -2,18 +2,22 @@ require 'yahoo-jp-ma-api/client'
 
 module YahooMA
     class << self
-        def new options={}
-            @client ||= YahooMA::Client.new options
-        end
 
         # override to delegate
         def method_missing name,*args,&block
+            @client ||= YahooMA::Client.new opts
             return super if @client.respond_to? name
             @client.send name,*args,&block
         end
 
         def respond_to? name,include_private=false
+            @client ||= YahooMA::Client.new opts
             @client.respond_to?(name,include_private) || super(name,include_private)
         end
+
+        def reset
+            @client = nil
+        end
+
     end
 end
